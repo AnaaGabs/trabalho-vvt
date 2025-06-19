7c3100d2ee0d00a0b14cd0403f736ec351372be3
ANA GABRIELA <g.ana@ufms.br>
2025-06-19 17:22:08 -0400

    Iniciando o teste automatizado do edital médio, seguindo as exigências na descriçãod o trabalho - erro ao digitar o texto do termo de aceite.

--- cypress/e2e/edital-medio.cy.ts
+++ cypress/e2e/edital-medio.cy.ts
@@ -10,18 +10,27 @@ describe('Sistema Integrado de Gestão para Fundações de Amparo a Pesquisas', 
       'Grupo11@sig', // [Senha do usuário]
     ); //Acessa a página de login usando as credenciais do usuário e senha.
   });
-  it.only('Realiza login no sistema e cria um edital simples', () => { //Teste edital simples, se houver mais de um teste, o it.only executa apenas esse teste.
+
+  it.only('Realiza login no sistema e cria um edital médio', () => { //Teste edital médio, se houver mais de um teste, o it.only executa apenas esse teste.
     cy.get('[data-cy="nav-group-edital"]').click(); //Clica na aba Editais
     cy.get('[data-cy="nav-item-publicar-edital"]').click(); //Clica na opção Editais para acessar da página de Editais
     cy.get('[data-cy="add-publicar-edital"]').click(); //Clica no botão "Adicionar" para criação de um novo Edital
     cy.get('[data-cy="nome"]').type(
-      'Grupo-11 E.S. 005/2025 ana-gabriela edital-simples', //Edite essa linha para preencher o nome do Edital
+      'Grupo-11 E.M. 005/2025 ana-barbosa', //Edite essa linha para preencher o nome do Edital
       { delay: 0 },
     ); //Preenche o campo "Nome" do Edital
     cy.get('[data-cy="restricoes"]').click(); //Clica na aba Restrições para seguir para a página de Restrições
     cy.get('[data-cy="definirDuracaoProjetoEmMeses"]').check(); //Marca a opção "Definir Duração do Projeto em Meses"
     cy.get('[data-cy="duracaoProjetoEmMeses"]').type('6'); //Preenche o campo "Duração do Projeto em Meses com o valor 6"
-    cy.get('[data-cy="pesquisadorSubmeterVariasPropostas"]').check(); //Marca a opção "Pesquisador pode submeter várias propostas"
+    cy.get('[data-cy="termo-de-aceite"]').click(); //Clica na aba de Termo de Aceite
+    cy.get('[data-cy="termoDeAceite"]').type(
+      'Termo de Aceite do Edital Médio', //Preenche o campo "Termo de Aceite" com um texto padrão
+    ); //Preenche o campo "Termo de Aceite" do Edital
+    cy.get('data.cy=texto-do-edital').click(); //Clica na aba Texto do Edital para seguir para a página de Texto do Edital
+    cy.get('[data-cy="texto"]').type('Texto do Edital Médio', { delay: 0 }); //Preenche o campo "Texto" do Edital com um texto padrão
+    cy.get('data-cy="abrangencia"').click(); //Clica na aba Abrangência para seguir para a página de Abrangência
+    cy.get('data.cy="estado-sao-paulo"').click(); //Clica no estado de São Paulo na lista de estados
+    cy.get('data.cy="estado-parana"').click(); //Clica no estado de São Paulo na lista de estados
     cy.get('[data-cy="cronograma"]').click(); //Clica na aba Cronograma para seguir para a página de Cronograma
     cy.get('[data-cy="periodo-de-submissao"]').click(); //Clica na aba Período de Submissão para seguir para a página de Período de Submissão
     cy.get('[data-cy="add-button"]').click(); //Clica no botão "Adicionar" para criar um novo Período de Submissão
@@ -31,13 +40,5 @@ describe('Sistema Integrado de Gestão para Fundações de Amparo a Pesquisas', 
     ); //Preenche o campo "Término" do Período de Submissão com a data do dia de hoje + 1 ano
     cy.get('[data-cy="chamada-confirmar"]').click(); //Clica no botão "Salvar" para salvar as informações do Período de Submissão
     cy.get('[data-cy="orcamento"]').click(); //Clica na aba Orçamento para exibir as opções de Orçamento
-    cy.get('[data-cy="programa"]').click(); //Clica em Programa para seguir para a página de Programa
-    cy.get('[data-cy="programaId"]').click(); //Clica no campo de seleção de Programa
-    cy.get('[data-cy-index="programaId-item-0"]').click(); //Seleciona o primeiro Programa da lista de Programas
-    cy.get('[data-cy="menu-salvar"]').click(); //Clica no botão "Salvar" para salvar as informações do Edital
-    cy.get('[data-cy="menu-finalizar"]').click(); //Clica no botão "Finalizar" para salvar e sair da área de adição do Edital
-
-    //Resultado esperado: O Edital deve ser salvo com sucesso e o usuário deve ser redirecionado para a página de Editais.
-  });
-
+    });
 });
