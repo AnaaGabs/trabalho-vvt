4a1c29b25d5b94d14c4eef89d999cd8463462b9d
ANA GABRIELA <g.ana@ufms.br>
2025-06-18 21:43:22 -0400

    initial commit

---
+++ cypress/support/commands.ts
@@ -0,0 +1,10 @@
+// cypress/support/commands.ts
+
+Cypress.Commands.add('typelogin', (url, username, password) => {
+  cy.visit(url);
+  cy.get('#login').type(username);
+  cy.get('#senha').type(password);
+  cy.get("form").within(() => {
+    cy.get("button[type='submit']").click();
+  });
+});
