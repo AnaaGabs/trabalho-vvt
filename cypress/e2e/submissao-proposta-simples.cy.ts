8720255fe6523e76eb9c6da2a785df77e7352f98
aline-hirokawa <alinelikahiro@gmail.com>
2025-07-01 15:58:20 -0400

    fix: corrigindo erro do endereço e finalizando teste de submissão de proposta simples.

--- cypress/e2e/submissao-proposta-simples.cy.ts
+++ cypress/e2e/submissao-proposta-simples.cy.ts
@@ -9,10 +9,16 @@ describe('Sistema Integrado de Gestão para Fundações de Amparo a Pesquisas', 
     ); //Acessa a página de login usando as credenciais do usuário e senha.
   });
   it('Realiza login no sistema e submete uma proposta', () => {
+    cy.get('[data-cy="user-menu"]').click(); //Clica no menu do usuário para acessar as opções de perfil
+    cy.get('[data-cy="editar-perfil"]').click(); //Clica na opção "Editar Perfil" para acessar a página de edição do perfil do usuário
+    cy.get('.ex40wuf1').click(); //Clica no botão "Próximo" para editar
+    cy.get('[data-cy="endereco.cep"]').type('79062-758'); //Preenche o campo "CEP" com um número fictício
+    cy.get('[data-cy="endereco.numero"]').type('2'); //Preenche o campo "Número" com um número fictício
+
     cy.get('[data-cy="breadcrumb-home"]').click(); //Clica no botão "Home" para retornar à página anterior
     cy.get('[data-cy="editais-ver-mais"]').click(); //Clica no botão "Ver Mais" para acessar a página de Editais
 
-    cy.get('[data-cy-index="visualizar-edital-grupo-11-e-s-005"]').click(); //Edite essa linha para selecionar o Edital respectivo
+    cy.get('[data-cy-index="visualizar-edital-9"]').click(); //Edite essa linha para selecionar o Edital respectivo
 
     cy.wait(300); //Aguarda 300ms para garantir que a página foi carregada completamente
     cy.get('[data-cy="criar-proposta"]').click(); //Clica no botão "Criar Proposta" para iniciar o processo de criação de uma nova proposta
@@ -42,8 +48,8 @@ describe('Sistema Integrado de Gestão para Fundações de Amparo a Pesquisas', 
     cy.get('[data-cy="criadoPor.documento"]').type('700.535.010-39'); //Preenche o campo "CPF" com um número fictício
     cy.get('[data-cy="next-button"]').click(); //Clica no botão "Próximo" para avançar para a próxima etapa do formulário de proposta
 
-    cy.get('[data-cy="criadoPor.endereco.cep"]').type('79002-052'); //Preenche o campo "CEP" com um número fictício
-    cy.get('[data-cy="criadoPor.endereco.numero"]').type('123'); //Preenche o campo "Número" com um número fictício
+    //cy.get('[data-cy="criadoPor.endereco.cep"]').type('79002-052'); //Preenche o campo "CEP" com um número fictício
+    //cy.get('[data-cy="criadoPor.endereco.numero"]').type('123'); //Preenche o campo "Número" com um número fictício
     cy.get('[data-cy="next-button"]').click(); //Clica no botão "Próximo" para avançar para a próxima etapa do formulário de proposta
 
     cy.get('[data-cy="criadoPor.instituicaoId"]').click(); //Clica no campo de seleção de Instituição
@@ -73,11 +79,12 @@ describe('Sistema Integrado de Gestão para Fundações de Amparo a Pesquisas', 
     cy.get('[data-cy="next-button"]').click(); //Clica no botão "Próximo" para avançar para a próxima etapa do formulário de proposta
     cy.get('[data-cy="next-button"]').click(); //Clica no botão "Próximo" para avançar para a próxima etapa do formulário de proposta
     cy.get('[data-cy="next-button"]').click(); //Clica no botão "Próximo" para avançar para a próxima etapa do formulário de proposta
-    cy.get('[data-cy="next-button"]').click(); //Clica no botão "Próximo" para avançar para a próxima etapa do formulário de proposta
+    //cy.get('[data-cy="next-button"]').click(); //Clica no botão "Próximo" para avançar para a próxima etapa do formulário de proposta
 
     cy.get('[data-cy="termoDeAceiteAceito"]').check(); //Marca a caixa de seleção "Termo de Aceite Aceito" para aceitar os termos e condições
     cy.get('[data-cy="menu-salvar"]').click(); //Clica no botão "Salvar" para salvar as informações da proposta
     cy.get('[data-cy="menu-verificar-penden"]').click(); //Clica no botão "Verificar Pendências" para verificar se há pendências na proposta
+    cy.get('.ex40wuf2').click(); //Clica no botão "Submeter Proposta" para enviar a proposta para análise
 
   }); //Cria uma nova proposta de acordo com o edital selecionado
 });
